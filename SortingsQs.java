0e0364a1f090f1b3946329a079f40d8549963579
RAHUL WARKADE <88571832+Rahulwarkade@users.noreply.github.com>
2023-01-28 21:23:22 +0530

    Sortings
    bubbleSort, selectionSor, insertionSort, countSort.

--- SortingsQs.java
+++ SortingsQs.java
@@ -91,6 +91,9 @@ public class SortingsQs
 	public static void main(String args[])
 	{
 		int arr[] = {3,6,2,1,8,7,4,5,3,1};
-		printArr(arr);
+		bubbleSort(arr);
+		selectionSort(arr);
+		insertionSort(arr);
+		countSort(arr);
 	}
 }
