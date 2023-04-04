f4bbc71b065561b4c8f3e1c44c7cc4a3ab7d65fe
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-04 08:48:28 +0530

    Linked ListsQ

--- Linked_ListQs.java
+++ Linked_ListQs.java
@@ -171,7 +171,7 @@ public class Linked_ListQs
 
 		obj2.printLL(obj2.head);
 		obj.intersection(obj.head,obj2.head,3);
-		// obj2.printLL(obj2.head);
+		obj2.printLL(obj2.head);
 		// System.out.println(obj.intersectPoint(obj.head,obj2.head));
 
 	}
